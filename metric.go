08a585b9bed85789a0a70dffbd235f7f818338dc
gdanichev <GDanichev@artenecy.ru>
2025-06-12 23:56:34 +0300

    filesystem_storage.go

--- metric.go
+++ metric.go
@@ -21,17 +21,17 @@ func NewStat(storage Storage) Storage {
 	return &stat{Storage: storage}
 }
 
-func (s stat) Get(ctx context.Context, key string) (Entry, bool, error) {
+func (s *stat) Get(ctx context.Context, key string) (Entry, bool, error) {
 	atomic.AddInt64(&s.getCmd, 1)
 	return s.Storage.Get(ctx, key)
 }
 
-func (s stat) Put(ctx context.Context, key string, outputID []byte, body io.Reader) (string, error) {
+func (s *stat) Put(ctx context.Context, key string, outputID []byte, body io.Reader) (string, error) {
 	atomic.AddInt64(&s.putCmd, 1)
 	return s.Storage.Put(ctx, key, outputID, body)
 }
 
-func (s stat) Close(ctx context.Context) error {
+func (s *stat) Close(ctx context.Context) error {
 	atomic.AddInt64(&s.closeCmd, 1)
 	fmt.Fprintf(os.Stderr, "getCmd=%d putCmd=%d closeCmd=%d\n",
 		atomic.LoadInt64(&s.getCmd),
